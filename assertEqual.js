31fa64553499a8e89014b626889f11421e5d9b37
Diana <slorrrr@gmail.com>
2021-11-22 20:40:55 -0500

    Assert Pass/Fail

---
+++ assertEqual.js
@@ -0,0 +1,16 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("🦖 Assertion Passed: " + `${actual} === ${expected}`);
+  } else {
+    console.log("🦑 Assertion Failed: " + actual + " !== " + expected);
+  }
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("cat", "cat");
+assertEqual("cat", "dog");
+assertEqual(10, 10);
+assertEqual(1, 19);
