efc6864515b0f83c8c78c8682d5a21610366ff7c
Diana <slorrrr@gmail.com>
2021-11-24 00:32:38 -0500

    new array equals fx

---
+++ assertArraysEqual.js
@@ -0,0 +1,17 @@
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  let compare = arr1.every(function(value, index) {
+    return arr1[index] === arr2[index];
+  });
+  return compare;
+}
+
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`🦖 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🦑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
