556e35a782477ca702811c7d14f7ea1979534775
Diana <slorrrr@gmail.com>
2021-11-25 22:53:31 -0500

    Object comparison

--- eqObjects.js
+++ eqObjects.js
@@ -27,11 +27,12 @@ const eqObjects = function(object1, object2) {
   }
 
   for (const key of obj1) {
-    if (Array.isArray(obj1[key]) && Array.isArray(obj2[key])) {
-      if (!eqArrays(obj1[key], obj2[key])) {
+    console.log(obj1[key]);
+    if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
+      if (!eqArrays(object1[key], object2[key])) {
         return false;
       }
-    } else if (obj1[key] !== obj2[key]) {
+    } else if (object1[key] !== object2[key]) {
       return false;
     }
   }
