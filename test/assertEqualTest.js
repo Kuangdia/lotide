32e163c6c8a0b6b738b050e9df56841e5f4090a9
Diana <slorrrr@gmail.com>
2021-11-29 15:59:30 -0500

    Assert test files using require

---
+++ test/assertEqualTest.js
@@ -0,0 +1,9 @@
+const assertEqual = require('../assertEqual');
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("cat", "cat");
+assertEqual("cat", "dog");
+assertEqual(10, 10);
+assertEqual(1, 19);
