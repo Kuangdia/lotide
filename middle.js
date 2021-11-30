ae588819faf89166a40803adb207642e02aa6e8d
Diana <slorrrr@gmail.com>
2021-11-29 20:36:26 -0500

    refactored middle.js with require and assert

--- middle.js
+++ middle.js
@@ -2,18 +2,25 @@ const eqArrays = require('./eqArrays');
 const assertArraysEqual = require('./assertArraysEqual');
 
 const middle = function(array) {
+  array.sort();
+
   let arr = [];
   if (array.length < 3) {
     return arr;
   }
 
-  let evenMid1 = array[Math.floor((array.length - 1) / 2)]
-  let evenMid2 = array[Math.round(array.length) / 2]
-  if (array.length % 2 !== 0) {
-    return [array[Math.round(array.length - 1) / 2]];
-  } else {
-    return [evenMid1, evenMid2];
+  const middleIndex = Math.floor(array.length / 2)
+  if (array.length % 2 === 0) {
+    const otherIndex = middleIndex - 1;
+    const middleOne = array[middleIndex];
+    const middleTwo = array[otherIndex];
+
+    return [middleTwo, middleOne];
   }
+    // if odd-length return middle number
+    // this returns the index of middle number
+    const targetValue = array[middleIndex];
+    return [targetValue];
 }
 
 module.exports = middle;
