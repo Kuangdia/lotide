b79a2126201865d56ad3e3a2a0bca9bb723e88ea
Diana <slorrrr@gmail.com>
2021-11-29 20:36:47 -0500

    testing middle.js with mocha and chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,19 +1,32 @@
-const middle = require('../middle');
-const eqArrays = require('../eqArrays');
-const assertArraysEqual = require('../assertArraysEqual');
+const assert = require('chai').assert;
+const middle   = require('../middle');
 
+describe("middle", () => {
+  it("returns empty arrays with 1-2 elements", () => {
+    assert.deepEqual(middle([1]), [])
+  });
 
-// test
-console.log(assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]));
+  it("returns middle of an array", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4])
+  });
 
-// Arrays with 1-2 elements have NO middle => return empty array
-console.log(middle([1])) // => []
-console.log(middle([1, 2])) // => []
+  it("return middle element with odd number array", () => {
+    assert.deepEqual(middle([1, 2, 3]), [2]);
+  });
 
-// Array with odd number of elements => return middle element
-console.log(middle([1, 2, 3])) // => [2]
-console.log(middle([1, 2, 3, 4, 5])) // => [3]
+})
 
-// Array with even number of elements => return 2 middle elements
-console.log(middle([1, 2, 3, 4])) // => [2, 3]
-console.log(middle([1, 2, 3, 4, 5, 6])) // => [3, 4]
+// // test
+// console.log(assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]));
+
+// // Arrays with 1-2 elements have NO middle => return empty array
+// console.log(middle([1])) // => []
+// console.log(middle([1, 2])) // => []
+
+// // Array with odd number of elements => return middle element
+// console.log(middle([1, 2, 3])) // => [2]
+// console.log(middle([1, 2, 3, 4, 5])) // => [3]
+
+// // Array with even number of elements => return 2 middle elements
+// console.log(middle([1, 2, 3, 4])) // => [2, 3]
+// console.log(middle([1, 2, 3, 4, 5, 6])) // => [3, 4]
