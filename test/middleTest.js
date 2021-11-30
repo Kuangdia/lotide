e8af9136757b06705b93102786217d2f9b3d31b0
Diana <slorrrr@gmail.com>
2021-11-29 21:00:48 -0500

    Testing middle.js using mocha and chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -14,19 +14,4 @@ describe("middle", () => {
     assert.deepEqual(middle([1, 2, 3]), [2]);
   });
 
-})
-
-// // test
-// console.log(assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]));
-
-// // Arrays with 1-2 elements have NO middle => return empty array
-// console.log(middle([1])) // => []
-// console.log(middle([1, 2])) // => []
-
-// // Array with odd number of elements => return middle element
-// console.log(middle([1, 2, 3])) // => [2]
-// console.log(middle([1, 2, 3, 4, 5])) // => [3]
-
-// // Array with even number of elements => return 2 middle elements
-// console.log(middle([1, 2, 3, 4])) // => [2, 3]
-// console.log(middle([1, 2, 3, 4, 5, 6])) // => [3, 4]
+});
