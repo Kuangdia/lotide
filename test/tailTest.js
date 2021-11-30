f1b0bba612e72f68cd4a8d2db38a1dd800384ef2
Diana <slorrrr@gmail.com>
2021-11-29 21:00:13 -0500

    Testing tail.js with mocha and chai

--- test/tailTest.js
+++ test/tailTest.js
@@ -17,17 +17,4 @@ describe("tail", () => {
     assert.deepEqual(tail(word), ["lala", "flora"]);
   })
 
-})
-
-// const assertEqual = require('../assertEqual');
-// const tail = require('../tail');
-
-// // Test Case: Check the original array 
-// const words = ["Yo Yo", "Lighthouse", "Labs"];
-// tail(words); // no need to capture the return value since we are not checking it
-// assertEqual(words.length, 3); // original array should still have 3 elements!
-
-
-// const word = ["bunny"];
-// let name1 = tail(word);
-// assertEqual(name1, 0);
+});
